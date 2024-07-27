2460b3a83d97896ae1cf891193c59bc31b93a06d
Janmajaya Mall <janmajayamall@protonmail.com>
2024-07-27 16:32:02 -0700

    test 2021/480

--- notes.md
+++ notes.md
@@ -1,19 +1,18 @@
-
 ## Polynomial multiplication using FFT
 
-Given polynomials $f(X)$ and $g(X)$ of degree $N$, one can output product polynomial $fg(X)$ in $N^2$ time. But turns out using FFT one can do this in $N log N$ time. 
+Given polynomials $f(X)$ and $g(X)$ of degree $N$, one can output product polynomial $fg(X)$ in $N^2$ time. But turns out using FFT one can do this in $N log N$ time.
 
-A simple observation plus the fact that one can evaluate any polynomial at different powers of $2N^{th}$ root of unity in $N log N$ time may hint something. 
+A simple observation plus the fact that one can evaluate any polynomial at different powers of $2N^{th}$ root of unity in $N log N$ time may hint something.
 
-The observation is "interpolation of $N$ data points into a polynomial of degree $N-1$ is unique".  With this observation and the fact that resulting polynomial will be degree $2N - 1$ we can first apply FFT transform to $f$ and $g$ with $2n^{th}$ root of unity (i.e. evaluate $f(k)$ and $g(k)$ for $k \in [0, \omega_{2n}, ..., \omega_{2n}^{2n-1}]$). Then we take the Hadamard product $FFT(f) \odot FFT(g)$ to produce $2N-1$ evaluation points for polynomial $fg(X)$. Observe that the resulting $2N-1$ points equal $fg(x)$ evaluated at $k \in [0, \omega_{2n}, ..., \omega_{2n}^{2n-1}]$. 
+The observation is "interpolation of $N$ data points into a polynomial of degree $N-1$ is unique". With this observation and the fact that resulting polynomial will be degree $2N - 1$ we can first apply FFT transform to $f$ and $g$ with $2n^{th}$ root of unity (i.e. evaluate $f(k)$ and $g(k)$ for $k \in [0, \omega_{2n}, ..., \omega_{2n}^{2n-1}]$). Then we take the Hadamard product $FFT(f) \odot FFT(g)$ to produce $2N-1$ evaluation points for polynomial $fg(X)$. Observe that the resulting $2N-1$ points equal $fg(x)$ evaluated at $k \in [0, \omega_{2n}, ..., \omega_{2n}^{2n-1}]$.
 
-To find coefficients of $fg(X)$ one can inverse FFT transform as $FFT^{-1}(FFT(f) \odot FFT(g))$. To understand why inverse transformation results in coefficients (i.e. is equivalent to interpolation) one may view FFT transform as matrix multiplication:
+To find coefficients of $fg(X)$ one can perform inverse transform as $FFT^{-1}(FFT(f) \odot FFT(g))$. To understand why inverse transformation results in coefficients (i.e. is equivalent to interpolation) one may view FFT transform as matrix multiplication:
 
 $$
-\begin{matrix}  
-1 & 1 & 1 & ... & 1 \\  
-\omega_{2n} & \omega_{2n} & \omega_{2n} & ... & \omega_{2n} \\  
-\omega_{2n}^2 & \omega_{2n}^2 & \omega_{2n}^2 & ... & \omega_{2n}^2 \\  
+\begin{matrix}
+1 & 1 & 1 & ... & 1 \\
+\omega_{2n} & \omega_{2n} & \omega_{2n} & ... & \omega_{2n} \\
+\omega_{2n}^2 & \omega_{2n}^2 & \omega_{2n}^2 & ... & \omega_{2n}^2 \\
 \vdots \\
 \omega_{2n}^{2n-1} & \omega_{2n}^{2n-1} & \omega_{2n}^{2n-1} & ... & \omega_{2n}^{2n-1}
 \end{matrix}
@@ -34,16 +33,18 @@ a_{2n-1}
 \hat{a_{2n-1}} \\
 \end{matrix}
 $$
+
 where $\omega_{2N}$ is $2N^{th}$ root of unity, $a_0, a_1,...a_{2n-1}$ are coefficients of polynomial $fg(X)$ and resulting column vector contains $fg(\omega_{2N}^k)$ at row $k$.
 
-The left most matrix has a special name called Vandermonde matrix. Let us denote it with $M$. Given that $FFT(f) \odot FFT(g)$ results in column vector on RHS, we can calculate the column vector consisting of coefficients by  $M^{-1} \cdot (FFT(f) \odot FFT(g))$. 
+The left most matrix has a special name called Vandermonde matrix. Let us denote it with $M$. Given that $FFT(f) \odot FFT(g)$ results in column vector on RHS, we can calculate the column vector consisting of coefficients by $M^{-1} \cdot (FFT(f) \odot FFT(g))$.
 
 Due to "Inversion theorem":
 $$M(\omega_{2N})^{-1} = \frac{1}{n}M(\omega_{2N}^{-1})$$
-Thus to calculate the coefficients one may calculate: 
+Thus to calculate the coefficients one may calculate:
+
 $$
-\begin{matrix}  
-1 & 1 & 1 & ... & 1 \\  
+\begin{matrix}
+1 & 1 & 1 & ... & 1 \\
 \omega_{2n}^{-1} & \omega_{2n}^{-1} & \omega_{2n}^{-1} & ... & \omega_{2n}^{-1} \\
 \omega_{2n}^{-2} & \omega_{2n}^{-2} & \omega_{2n}^{-2} & ... & \omega_{2n}^{-2} \\
 \vdots \\
@@ -69,53 +70,57 @@ $$
 
 The structure above corresponds to FFT transformation on vector $FFT(f) \odot FFT(g)$ with $\omega_{2n}^{-1}$ instead of $\omega_{2n}$. Thus one can find the coefficients of resulting polynomial by evaluating $FFT^{-1}(FFT(f) \odot FFT(g))$ where $FFT^{-1}$ is FFT algorithm with $\omega_{2n}^{-1}$.
 
-Reference: 
+Reference:
 https://www.cs.toronto.edu/~denisp/csc373/docs/tutorial3-adv-writeup.pdf
 
 ## Cyclic multiplication using FFT
 
-Let $f$ and $g$ be vectors. We denote the result $FFT^{-1}(FFT(f) \odot FFT(g))$ as convolution of vectors $f$ and $g$. 
+Let $f$ and $g$ be vectors. We denote the result $FFT^{-1}(FFT(f) \odot FFT(g))$ as convolution of vectors $f$ and $g$.
 
-The resulting vector equals: 
+The resulting vector equals:
 $$\sum_{k=0}^{N} a_kb_{j-k\mod{N}}$$
-at $j^{th}$ row, where $a_k$ are coefficients of $f$ and $b_{k}$ are coefficients of $g$. 
+at $j^{th}$ row, where $a_k$ are coefficients of $f$ and $b_{k}$ are coefficients of $g$.
 
 This is equivalent to cyclic multiplication $\mod {X^N - 1}$.
 
 ## Negacyclic multiplication using FFT
 
-For modulo $X^N + 1$ (i.e. negacyclic multiplication) one cannot directly use FFT because the terms wrap around and negate. 
+For modulo $X^N + 1$ (i.e. negacyclic multiplication) one cannot directly use FFT because the terms wrap around and negate.
+
 ### Double trick
 
-Notice that $(X^{2N} - 1)$ can be factorised into $(X^N-1)$ and $(X^N+1)$. This implies, due to Chinese remainder theorem, that each value in modulo $X^{2N} -1$ has unique representation in modulo $X^N+1$. 
+Notice that $(X^{2N} - 1)$ can be factorised into $(X^N-1)$ and $(X^N+1)$. This implies, due to Chinese remainder theorem, that each value in modulo $X^{2N} -1$ has unique representation in modulo $X^N+1$.
 
 If one can re-write (pre-image map) a value $\in Z[X]/X^N+1$ as a unique value $\in Z[X]/X^{2N}-1$, then they can perform cyclic multiplication and reduce the result to $Z[X]/X^N+1$.
 
 Let's define the pre-image mapping as: $f(X)(1-X^N)$, where $f(X) \in Z[X]/X^N+1$
 
-Reducing $f(X)-X^Nf(x)) \in Z[X]/X^{2N}-1$ to $Z[X]/X^{N}+1$ will negate the upper half coefficients of polynomial (since $X^N = -1$ and lower half coefficients a variable exponents smaller than $N$), thus resulting in $f(X) + f(X) = 2f(X)$. 
+Reducing $f(X)-X^Nf(X)) \in Z[X]/X^{2N}-1$ to $Z[X]/X^{N}+1$ will negate the upper half coefficients of polynomial (since $X^N = -1$ and lower half coefficients have exponents smaller than $N$), thus resulting in $f(X) + f(X) = 2f(X)$.
 
-To calculate $f(X)g(X) \mod{X^N+1}$, we pre-image map $f(X)$ and $g(X)$ as $f'(X) = f(X) - f(X)X^N, \space g'(X) = g(X) - g(X)X^N$ $\in Z[X]/X^{2N-1}$. Pre-image mapping can be done easily by negating coefficient vector of polynomial and append it to itself. 
+To calculate $f(X)g(X) \mod{X^N+1}$, we pre-image map $f(X)$ and $g(X)$ as $f'(X) = f(X) - f(X)X^N, \space g'(X) = g(X) - g(X)X^N$ $\in Z[X]/X^{2N-1}$. Pre-image mapping can be done easily by negating coefficient vector of polynomial and append it to itself.
 
 We calculate $f'(X)g'(X)$ using FFT and reduce the result $\mod{X^N+1}$. Now notice that the resulting value equals
 $$f'(X)g'(X) \mod X^N+1 = 2f(X) \cdot 2g(X) = 4fg$$
-Thus we divide the reduced polynomial in $Z[X]/X^N+1$ by 4. 
+Thus we divide the reduced polynomial in $Z[X]/X^N+1$ by 4.
 
-Reduction at the end is trivial if one observers the following: 
+Reduction at the end is trivial if one observers the following:
 $$f'(X)g'(X)= -f(X)(X^N-1) \cdot -g(X)(X^N-1)$$
 $$f(X)g(X)(X^N-1)^2 = f(X)g(X)(X^{2N} + 1 - 2X^N) = 2(f(X)g(X) - f(X)g(X)X^N)$$
-This implies the resulting product $\in Z[X]/X^{2N}-1$ is pre-image mapping of product $\in Z[X]/X^N+1$. Thus  to reduce. one can invert the per-image image. That is truncate upper half of coefficient vector. Then we divide by 2 to get $f(X)g(X)$.
+This implies the resulting product $\in Z[X]/X^{2N}-1$ is pre-image mapping of product $\in Z[X]/X^N+1$. Thus to reduce. one can invert the per-image image. That is truncate upper half of coefficient vector. Then we divide by 2 to get $f(X)g(X)$.
+
 ### Twisting trick
 
 $X^N+1$ can be factorised into co-prime factors $(X^{N/2}+i) (X^{N/2}-i)$. This implies every value in $X^{N}+1$ has unique representation $X^{N/2}-i$. Hence, one can calculate negacyclic polynomial multiplication in ring $\mod {X^{N/2}-i}$ and map it back to ring $\mod{X^N+1}$.
 
-But FFTs work directly only on rings with structure $Z[X]/X^k-1$ (i.e. cyclic polynomials). Thus we need a map from ring $\mod {X^{N/2}} + i$ to ring $\mod X^{N/2}-1$. Turns a trivial map exists if we map $X$ of $g(X) \in Z[X]/X^{N/2}-i$ to $\omega_{4(N/2)}X$. 
+But FFTs work directly only on rings with structure $Z[X]/X^k-1$ (i.e. cyclic polynomials). Thus we need a map from ring $\mod {X^{N/2}} + i$ to ring $\mod X^{N/2}-1$. A trivial map exists if we map $X$ of $g(X) \in Z[X]/X^{N/2}-i$ to $\omega_{4(N/2)}X$.
 
-To see why, let: 
+To see why, let:
+
 $$
 f(X) = g(X) + h(X)(X^{N/2}-i)
 $$
-where $h(X)$ is quotient and $f(X)$ is original polynomial. 
+
+where $h(X)$ is quotient and $f(X)$ is original polynomial.
 
 Mapping $X$ to $\omega_{4(N/2)}X$:
 $$f(\omega_{4(N/2)}X) = g(\omega_{4(N/2)}X) + h(\omega_{4(N/2)}X) ((\omega_{4(N/2)}X)^{N/2}-i)$$
@@ -125,17 +130,14 @@ Since $e^{\frac{\pi i}{2}} = i$,
 $$f(\omega_{4(N/2)}X) = g(\omega_{4(N/2)}X) + ih(\omega_{4(N/2)}X) (X^{N/2}-1)$$
 $$f(\omega_{4(N/2)}X) = g(\omega_{4(N/2)}X) \mod{(X^{N/2}-1)}$$
 
-Mapping $X^N+1$ to $X^{N/2}-i$ can be done by interpreting $X^{N/2} = i$. This implies, to reduce an element $\in Z[X]/X^N+1$ to an element in $Z[X]/X^{N/2} - i$, multiply all coefficients in upper half by $i$ and then halve their exponents and add them to their corresponding coefficients in lower half. Thus, the upper half coefficient vector becomes the imaginary parts of lower half coefficient vector. 
+Mapping $X^N+1$ to $X^{N/2}-i$ can be done by interpreting $X^{N/2} = i$. This implies, to reduce an element $\in Z[X]/X^N+1$ to an element in $Z[X]/X^{N/2} - i$, multiply all coefficients in upper half by $i$ and then halve their exponents and add them to their corresponding coefficients in lower half. Thus, the upper half coefficient vector becomes the imaginary parts of lower half coefficient vector.
 
-For second mapping, observe that 
+For second mapping, observe that
 $$g(\omega_{4(N/2)}X) = a_0 + a_1(\omega_{4(N/2)}X) + a_2(\omega_{4(N/2)}X)^2 + ... + a_{(N/2)-1}(\omega_{4(N/2)}X)^{(N/2)-1}$$
 $$= a_0 + \omega_{4(N/2)}a_1(X) + \omega_{4(N/2)}^2a_2(X)^2 + ... + \omega_{4(N/2)}^{(N/2)-1}a_{(N/2)-1}(X)^{(N/2)-1}$$
-One can simply map $g(X)$ to $g(\omega_{4(N/2)}X)$ by multiplying all coefficients element-wise with vector: 
+One can simply map $g(X)$ to $g(\omega_{4(N/2)}X)$ by multiplying all coefficients element-wise with vector:
 $$[1, \omega_{4(N/2)},\omega_{4(N/2)}^2, ..., \omega_{4(N/2)}^{(N/2)-1}]$$
 
-After mapping $f,g \in Z[X]/X^N+1$ to element in $Z[X]/X^{N/2}-1$, one can perform FFT to get the product in $Z[X]/X^{N/2}-1$. Then they can invert the maps to get product in $Z[X]/X^{N}+1$. 
+After mapping $f,g \in Z[X]/X^N+1$ to element in $Z[X]/X^{N/2}-1$, one can perform FFT to get the product in $Z[X]/X^{N/2}-1$. Then they can invert the maps to get product in $Z[X]/X^{N}+1$.
 
-Inverse map for the second mapping is as simple as multiplying the coefficients of element with inverse of $\omega_{4(N/2)}$ powers vector. And inverse mapping from $Z[X]/X^{N/2}-i$ to $Z[X]/X^N+1$ is reverse of mapping other way around. That is, take the imaginary part of coefficients and append them to the coefficient array as real parts for upper half. 
-
-
-
+Inverse map for the second mapping is as simple as multiplying the coefficients of element with inverse of $\omega_{4(N/2)}$ powers vector. And inverse mapping from $Z[X]/X^{N/2}-i$ to $Z[X]/X^N+1$ is reverse of mapping other way around. That is, take the imaginary part of coefficients and append them to the coefficient array as real parts for upper half.
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,13 @@
-use rand::{thread_rng, Rng};
 use rustfft::{
     num_complex::{Complex, ComplexFloat},
     num_traits::Pow,
     FftPlanner,
 };
 
-fn school_book_cylic_multiplication(p1: &[f32], p2: &[f32]) -> Vec<f32> {
+/// School book negacylic mult in ring Z[X]/(X^N+1)
+fn school_book_negacylic_multiplication(p1: &[f64], p2: &[f64]) -> Vec<f64> {
     let n = p1.len();
-    let mut res = vec![0f32; n];
-    for i in 0..n {
-        for j in 0..i + 1 {
-            res[i] += p1[j] * p2[i - j];
-        }
-        for j in (i + 1)..n {
-            res[i] += p1[j] * p2[n - (j - i)];
-        }
-    }
-    res
-}
-
-fn school_book_negacylic_multiplication(p1: &[f32], p2: &[f32]) -> Vec<f32> {
-    let n = p1.len();
-    let mut res = vec![0f32; n];
+    let mut res = vec![0f64; n];
     for i in 0..n {
         for j in 0..i + 1 {
             res[i] += p1[j] * p2[i - j];
@@ -34,26 +20,61 @@ fn school_book_negacylic_multiplication(p1: &[f32], p2: &[f32]) -> Vec<f32> {
     res
 }
 
-fn normalize_fft_output(p: &mut [Complex<f32>]) {
-    let n = p.len() as f32;
+/// School book negacylic mul in ring Z_{q}[X]/X^N+1
+fn school_book_negacylic_multiplication_mod_z_q(p1: &[u64], p2: &[u64], q: u64) -> Vec<u64> {
+    let n = p1.len();
+    let mut res = vec![0; n];
+    for i in 0..n {
+        for j in 0..i + 1 {
+            res[i] = (res[i] + ((p1[j] as u128) * (p2[i - j] as u128) % (q as u128)) as u64) % q;
+        }
+
+        for j in (i + 1)..n {
+            res[i] = (res[i] + q
+                - ((p1[j] as u128) * (p2[n - (j - i)] as u128) % (q as u128)) as u64)
+                % q;
+        }
+    }
+    res
+}
+
+/// School book negacylic mul in ring Z_{q=u64}[X]/X^N+1
+fn school_book_negacylic_multiplication_native_mod(p1: &[u64], p2: &[u64]) -> Vec<u64> {
+    let n = p1.len();
+    let mut res = vec![0u64; n];
+    for i in 0..n {
+        for j in 0..i + 1 {
+            res[i] = res[i].wrapping_add(p1[j].wrapping_mul(p2[i - j]));
+        }
+
+        for j in (i + 1)..n {
+            res[i] = res[i].wrapping_sub(p1[j].wrapping_mul(p2[n - (j - i)]));
+        }
+    }
+    res
+}
+
+fn normalize_fft_output(p: &mut [Complex<f64>]) {
+    let n = p.len() as f64;
     p.iter_mut().for_each(|v| *v = *v / n);
 }
-/// Multiplies (p1 x p2) mod X^N+1
-fn negacylic_mul_with_twist(p1: &[f32], p2: &[f32]) -> Vec<f32> {
+
+/// Multiplies (p1 x p2) mod X^N+1 using FFNT (https://eprint.iacr.org/2021/480.pdf)
+fn negacylic_mul_with_twist(p1: &[f64], p2: &[f64]) -> Vec<f64> {
     /// Maps $p \in Z[X]/X^N+1$ to element $\in Z[X]/X^N-1$. One cannot map $p$ directly
     /// from big ring to small ring. Instead observe that X^N+1 factors into X^{N/2}-i and X^{N/2}+i.
     /// Ring $Z[X]/X^{N/2}-i$ maps to Z[X]/X^{N/2}-1 by replacing X with $\omega_{4(N/2)}X$
     /// (refer to accompaniying notes to understand why).
     ///
     /// Thus to map $p$ to element in smaller ring, first map it to X^N+1 by consdering upper half coefficients as imaginery counterparts of lower half coefficients. Then element-wise multiply resultling coefficient vector with vector of powers of $\omega_{4(N/2)}$.
-    fn _map_from_big_ring_to_small_ring(p: &[f32]) -> Vec<Complex<f32>> {
+    fn _map_from_big_ring_to_small_ring(p: &[f64]) -> Vec<Complex<f64>> {
         let n = p.len();
         let middle = p
             .iter()
             .take(p.len() / 2)
             .zip(p.iter().skip(p.len() / 2))
             .map(|(r, i)| Complex { re: *r, im: *i })
-            .collect::<Vec<Complex<f32>>>();
+            .collect::<Vec<Complex<f64>>>();
 
         let powers_of_unity = _powers_of_root_of_unity(n);
 
@@ -61,26 +82,26 @@ fn negacylic_mul_with_twist(p1: &[f32], p2: &[f32]) -> Vec<f32> {
             .iter()
             .zip(powers_of_unity.iter())
             .map(|(v, omega_power)| v * omega_power)
-            .collect::<Vec<Complex<f32>>>()
+            .collect::<Vec<Complex<f64>>>()
     }
 
     /// Maps $p \in Z[X]/X^N-1$ to element $\in Z[X]/X^N+1$.
     /// This calculates the inverse of `_map_from_big_ring_to_small_ring`. It first multiplies $p$ with inverse of powers of $\omega_{4(N/2)}$. Then to map
     /// resulting element in $Z[X]/X^N-i$ to element in $Z[X]/X^N+1$, it takes reals in coefficient vector and places them in lower half of new coefficient vector.
     /// Then it considers imaginary counterparts as reals and appends them to the new coefficient vector.
-    fn _map_from_small_ring_to_big_ring(p: &[Complex<f32>]) -> Vec<f32> {
+    fn _map_from_small_ring_to_big_ring(p: &[Complex<f64>]) -> Vec<f64> {
         let n = p.len() * 2;
 
         let power_of_unity_inverse = _powers_of_root_of_unity(n)
             .iter()
             .map(|c| c.inv())
-            .collect::<Vec<Complex<f32>>>();
+            .collect::<Vec<Complex<f64>>>();
 
         let middle = p
             .iter()
             .zip(power_of_unity_inverse.iter())
             .map(|(v, omega_power)| v * omega_power)
-            .collect::<Vec<Complex<f32>>>();
+            .collect::<Vec<Complex<f64>>>();
 
         let mut res = vec![];
         middle.iter().for_each(|v| {
@@ -92,18 +113,18 @@ fn negacylic_mul_with_twist(p1: &[f32], p2: &[f32]) -> Vec<f32> {
         res
     }
 
-    /// Calculates e^{(2i\pi)/4(k/2)}
-    fn _root_of_unity(k: usize) -> Complex<f32> {
-        Complex::from_polar(1.0, (std::f32::consts::PI / (k as f32)))
+    /// Calculates e^{(2i \pi)/2k}
+    fn _root_of_unity(k: usize) -> Complex<f64> {
+        Complex::from_polar(1.0, (std::f64::consts::PI / (k as f64)))
     }
 
     /// Calculates powers (\omega_{4(k/2)})^i for i \in [0,k/2)
-    fn _powers_of_root_of_unity(k: usize) -> Vec<Complex<f32>> {
+    fn _powers_of_root_of_unity(k: usize) -> Vec<Complex<f64>> {
         let root = _root_of_unity(k);
         (0..(k / 2))
             .into_iter()
-            .map(|exp| root.pow(&(exp as f32)))
-            .collect::<Vec<Complex<f32>>>()
+            .map(|exp| root.pow(&(exp as f64)))
+            .collect::<Vec<Complex<f64>>>()
     }
 
     let mut mapped_p1 = _map_from_big_ring_to_small_ring(&p1);
@@ -119,7 +140,7 @@ fn negacylic_mul_with_twist(p1: &[f32], p2: &[f32]) -> Vec<f32> {
         .iter()
         .zip(mapped_p2.iter())
         .map(|(v1, v2)| v1 * v2)
-        .collect::<Vec<Complex<f32>>>();
+        .collect::<Vec<Complex<f64>>>();
     let fft = planner.plan_fft_inverse(mapped_p1.len());
     fft.process(&mut product);
     normalize_fft_output(&mut product);
@@ -127,118 +148,141 @@ fn negacylic_mul_with_twist(p1: &[f32], p2: &[f32]) -> Vec<f32> {
     _map_from_small_ring_to_big_ring(&product)
 }
 
-/// Multiplies (p1 x p2) / (X^N + 1)
-fn negacylic_mul_duplicate_mapping(p1: &[f32], p2: &[f32]) -> Vec<f32> {
-    /// Maps $p \in Z[X]/X^N+1$ to $p-p(X^N) \in Z[X]/X^{2N}-1$. Since the upper half coefficients
-    /// in Big ring are -ve of lower half coefficients, mapping equals negative lower half coefficients
-    /// and append at the end.
-    fn _preimage_map_big_ring(p: &[f32]) -> Vec<Complex<f32>> {
-        let mut buffer1 = p
-            .iter()
-            .map(|v| Complex { re: *v, im: 0f32 })
-            .collect::<Vec<Complex<f32>>>();
-
-        p.iter()
-            .for_each(|v| buffer1.push(Complex { re: -*v, im: 0f32 }));
-
-        buffer1
-    }
-
-    /// Reduces product of $f(1-f(x^N))g(1-g(x^N)) \in Z[X]/X^{2N}-1$ where $f,g \in Z[X]/X^N+1$ to element $\in Z[X]/X^{N}+1$. Observing
-    /// $f(1-f(x^N))g(1-g(x^N)) = 2(fg-fg(X^N))$ where $fg \in Z[X]/X^{N}+1$, fg can be obtained from product by taking lower half of coefficient vector
-    /// and dividing the coefficients by 2.
-    fn _reduce_big_ring_product_to_small_ring(p: &[Complex<f32>]) -> Vec<f32> {
-        let res = p
-            .iter()
-            .take(p.len() / 2)
-            .map(|v| (0.5 * v.re).round())
-            .collect::<Vec<f32>>();
-        res
-    }
-
-    let mut buffer1 = _preimage_map_big_ring(p1);
-    let mut buffer2 = _preimage_map_big_ring(p2);
-
-    let mut planner = FftPlanner::new();
-    let fft = planner.plan_fft_forward(buffer1.len());
-    fft.process(&mut buffer1);
-    fft.process(&mut buffer2);
-
-    // Hadamard product
-    let mut product = buffer1
-        .iter()
-        .zip(buffer2.iter())
-        .map(|(v1, v2)| v1 * v2)
-        .collect::<Vec<Complex<f32>>>();
-
-    let fft = planner.plan_fft_inverse(buffer1.len());
-    fft.process(&mut product);
-    normalize_fft_output(&mut product);
-
-    _reduce_big_ring_product_to_small_ring(&product)
-}
-
-/// Multiplies (p1 x p2) / (X^N - 1)
-fn cyclic_mul(p1: &[f32], p2: &[f32]) -> Vec<f32> {
-    let mut planner = FftPlanner::new();
-    let mut buffer1 = p1
-        .iter()
-        .map(|v| Complex { re: *v, im: 0.0 })
-        .collect::<Vec<Complex<f32>>>();
-    let mut buffer2 = p2
-        .iter()
-        .map(|v| Complex { re: *v, im: 0.0 })
-        .collect::<Vec<Complex<f32>>>();
-
-    let fft = planner.plan_fft_forward(p1.len());
-    fft.process(&mut buffer1);
-    fft.process(&mut buffer2);
-
-    // Hadamard product
-    let mut product = buffer1
-        .iter()
-        .zip(buffer2.iter())
-        .map(|(v1, v2)| v1 * v2)
-        .collect::<Vec<Complex<f32>>>();
-
-    let fft = planner.plan_fft_inverse(p1.len());
-    fft.process(&mut product);
-    normalize_fft_output(&mut product);
-
-    product
-        .iter()
-        .map(|complex_v| complex_v.re())
-        .collect::<Vec<f32>>()
-}
-
 #[cfg(test)]
 mod tests {
+    use std::fmt::Debug;
+
+    use itertools::{izip, Itertools};
+    use rand::{thread_rng, Rng};
+    use rustfft::num_traits::ToPrimitive;
+
     use super::*;
+
+    pub(crate) struct Stats<T> {
+        pub(crate) samples: Vec<T>,
+    }
+
+    impl<T> Default for Stats<T> {
+        fn default() -> Self {
+            Stats { samples: vec![] }
+        }
+    }
+
+    impl<T: Copy + ToPrimitive + Debug> Stats<T>
+    where
+        // T: for<'a> Sum<&'a T>,
+        T: for<'a> std::iter::Sum<&'a T> + std::iter::Sum<T>,
+    {
+        pub(crate) fn new() -> Self {
+            Self { samples: vec![] }
+        }
+
+        pub(crate) fn mean(&self) -> f64 {
+            self.samples.iter().sum::<T>().to_f64().unwrap() / (self.samples.len() as f64)
+        }
+
+        pub(crate) fn variance(&self) -> f64 {
+            let mean = self.mean();
+
+            // diff
+            let diff_sq = self
+                .samples
+                .iter()
+                .map(|v| {
+                    let t = v.to_f64().unwrap() - mean;
+                    t * t
+                })
+                .into_iter()
+                .sum::<f64>();
+
+            diff_sq / (self.samples.len() as f64 - 1.0)
+        }
+
+        pub(crate) fn std_dev(&self) -> f64 {
+            self.variance().sqrt()
+        }
+
+        pub(crate) fn add_many_samples(&mut self, values: &[T]) {
+            self.samples.extend(values.iter());
+        }
+
+        pub(crate) fn add_sample(&mut self, value: T) {
+            self.samples.push(value)
+        }
+
+        pub(crate) fn merge_in(&mut self, other: &Self) {
+            self.samples.extend(other.samples.iter());
+        }
+    }
+
+    fn map_f64_to_z_q(values: &[f64], q: u64) -> Vec<u64> {
+        values
+            .iter()
+            .map(|v| {
+                // v.to_bits() % q
+                let vint = v.rem_euclid(q as f64).to_i64().unwrap() % (q as i64);
+                if vint.is_negative() {
+                    q - (vint.abs().to_u64().unwrap())
+                } else {
+                    vint.abs().to_u64().unwrap()
+                }
+            })
+            .collect()
+    }
+
+    fn map_to_i64_from_z_q(values: &[u64], q: u64) -> Vec<i64> {
+        values
+            .iter()
+            .map(|v| {
+                if *v > (q >> 1) {
+                    (q - v) as i64
+                } else {
+                    *v as i64
+                }
+            })
+            .collect()
+    }
 
     #[test]
     fn test_nega_cylic_and_cylic_mul() {
         let mut rng = thread_rng();
-        let n = 8;
+        let logn = 11;
+        let n = 1 << logn;
+        let loga = 50;
+        let logb = 17;
+        let q = 1u64 << loga;
 
         let mut p1 = vec![];
         let mut p2 = vec![];
 
-        for _ in 0..n {
-            p1.push(rng.gen_range(0f32..8.0).round());
-            p2.push(rng.gen_range(0f32..8.0).round());
+        let mut stats = Stats::new();
+
+        const K: usize = 1;
+
+        for _ in 0..K {
+            for _ in 0..n {
+                p1.push(rng.gen_range(0u64..1u64 << loga));
+                p2.push(rng.gen_range(0u64..1u64 << logb));
+            }
+
+            let p1_f64 = p1.iter().map(|v| v.to_f64().unwrap()).collect::<Vec<f64>>();
+            let p2_f64 = p2.iter().map(|v| v.to_f64().unwrap()).collect::<Vec<f64>>();
+
+            let result = map_f64_to_z_q(&negacylic_mul_with_twist(&p1_f64, &p2_f64), q);
+            let expected_result = school_book_negacylic_multiplication_mod_z_q(&p1, &p2, q);
+            let diff = izip!(result.iter(), expected_result.iter())
+                .map(|(a, b)| (a + q - b) % q)
+                .collect_vec();
+
+            stats.add_many_samples(&map_to_i64_from_z_q(&diff, q));
         }
 
-        // let p1 = vec![2.0, 5.0, 3.0, 1.0, 1.0, 4.0, 1.0, 4.0];
-        // let p2 = vec![5.0, 4.0, 5.0, 1.0, 3.0, 5.0, 1.0, 2.0];
-
-        let result1 = negacylic_mul_duplicate_mapping(&p1, &p2);
-        let result2 = negacylic_mul_with_twist(&p1, &p2);
-        let expected_result = school_book_negacylic_multiplication(&p1, &p2);
-        assert_eq!(result1, expected_result);
-        assert_eq!(result2, expected_result);
-
-        let result = cyclic_mul(&p1, &p2);
-        let expected_result = school_book_cylic_multiplication(&p1, &p2);
-        assert_eq!(result, expected_result);
+        println!("Error Mean: {}", stats.mean().abs().log2());
+        println!("Error log 2 std_dev: {}", stats.std_dev().abs().log2());
+        // Eq 35 2021/480
+        println!(
+            "Expected error log 2 std_dev <= {}",
+            (4 * logn + 2 * loga + 2 * logb - 2 * 53 - 3) / 2
+        );
     }
 }
